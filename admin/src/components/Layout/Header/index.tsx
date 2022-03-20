c78cb929997c51b71d44881ca0832c3f77f35783
onfuns <wanrenya@gmail.com>
2022-03-20 13:14:41 +0800

    feat: update

--- admin/src/components/Layout/Header/index.tsx
+++ admin/src/components/Layout/Header/index.tsx
@@ -1,10 +1,18 @@
 import { Menu, Dropdown } from 'antd'
 import { getLocalUser, logoutUser } from '@/actions/user'
 import styles from './style.less'
+import { LogoutOutlined } from '@ant-design/icons'
+import { useHistory } from 'umi'
 
 export default () => {
-  const { userName } = getLocalUser()
+  const history = useHistory()
 
+  const onLogout = () => {
+    logoutUser()
+    history.push('/login')
+  }
+
+  const { userName } = getLocalUser()
   return (
     <div className={styles.header}>
       <div className={styles.logoText}>管理后台</div>
@@ -12,7 +20,10 @@ export default () => {
         overlay={
           <Menu>
             <Menu.Item key={1} className={styles.menuItem}>
-              <a onClick={logoutUser}>退出</a>
+              <a onClick={onLogout}>
+                <LogoutOutlined style={{ marginRight: 5 }} />
+                退出
+              </a>
             </Menu.Item>
           </Menu>
         }
