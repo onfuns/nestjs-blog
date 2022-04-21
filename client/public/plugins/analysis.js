005ab2857015381394d7fc7add4280dcb39453a5
onfuns <wanrenya@gmail.com>
2022-04-21 10:01:41 +0800

    feat: update

--- client/public/plugins/analysis.js
+++ client/public/plugins/analysis.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || []
 ;(function () {
   var hm = document.createElement('script')
-  hm.src = 'https://hm.baidu.com/hm.js?81b53f414f21835a3015a1f347ea5ff7'
+  hm.src = 'https://hm.baidu.com/hm.js?132b2fc811b124ac6925b1eb38ba21d8'
   var s = document.getElementsByTagName('script')[0]
   s.parentNode.insertBefore(hm, s)
 })()
