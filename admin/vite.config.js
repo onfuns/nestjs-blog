f7aaab6d2f4ecb08706f7c92fbd638f05b4c4413
wanrenya <wanrenya@corp.netease.com>
2022-09-22 14:40:23 +0800

    feat: update

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -3,13 +3,19 @@ import react from '@vitejs/plugin-react'
 import vitePluginImportus from 'vite-plugin-importus'
 import theme from './theme'
 import { join } from 'path'
-import { readFile } from 'fs/promises'
 
 export default defineConfig({
   resolve: {
-    alias: {
-      '@/': join(__dirname, './src/'),
-    },
+    alias: [
+      {
+        find: /^~/,
+        replacement: '',
+      },
+      {
+        find: '@',
+        replacement: join(__dirname, './src'),
+      },
+    ],
   },
   server: {
     proxy: {
@@ -44,36 +50,10 @@ export default defineConfig({
         style: 'less',
       },
     ]),
-    {
-      // 升级antd 5 后删除
-      name: 'add-missing-theme',
-      apply: 'build',
-      transform: (code, id) => {
-        const reg = /antd(?:\/|\\{1,2})es(?:\/|\\{1,2})index\.js/
-        if (reg.test(id)) {
-          return `${code}\nexport const theme = undefined;`
-        }
-        return code
-      },
-    },
   ],
   optimizeDeps: {
     esbuildOptions: {
-      plugins: [
-        {
-          // 升级antd 5 后删除
-          name: 'add-missing-theme',
-          setup(build) {
-            build.onLoad({ filter: /antd(?:\/|\\{1,2})es(?:\/|\\{1,2})index\.js/ }, async args => {
-              const text = await readFile(args.path, 'utf8')
-              return {
-                contents: `${text}\nexport const theme = undefined;`,
-                loader: 'js',
-              }
-            })
-          },
-        },
-      ],
+      plugins: [],
     },
   },
 })
