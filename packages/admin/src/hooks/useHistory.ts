c588a9446b7e8d89e1a45a55a023e4b6fe422905
onfuns <wanrenya@gmail.com>
2023-05-15 18:20:23 +0800

    feat: update

--- packages/admin/src/hooks/useHistory.ts
+++ packages/admin/src/hooks/useHistory.ts
@@ -2,6 +2,8 @@ import {
   createSearchParams,
   useLocation,
   useNavigate,
+  useParams,
+  useSearchParams,
   type NavigateOptions,
   type To as NavigateTo,
 } from 'react-router-dom'
@@ -9,9 +11,13 @@ import {
 export const useHistory = () => {
   const location = useLocation()
   const navigate = useNavigate()
+  const [searchParams] = useSearchParams()
+  const params = useParams()
   return {
     location,
     push: (to: NavigateTo, options?: NavigateOptions) => navigate(to, options),
     searchToString: (search: string) => createSearchParams(search).toString(),
+    searchParams,
+    params,
   }
 }
